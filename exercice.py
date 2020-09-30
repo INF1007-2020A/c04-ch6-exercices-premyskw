6e17d4ce7fac0ec106bade8b0b2a50cd2a618f63
Rémy S <70984250+premyskw@users.noreply.github.com>
2020-09-29 22:03:48 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,61 +3,80 @@
 
 def order(values: list = None) -> list:
     if values is None:
-        values = []
+        # TODO: demander les valeurs ici
         for i in range(9):
-            valeur = float(input())
-            values.append(valeur)
+            value = float(input("Entrez une valeur: "))
+            values.append(value)
         values.sort()
+
     return values
+
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        word1 = str(input())
-        word2 = str(input())
+        # TODO: demander les mots ici
+        mot1 = str(input())
+        mot2 = str(input())
         i = 0
-        for char in word1:
-            i -= 1
-            if char == word2[i]:
-                pass
-            else:
-                return False
+        for char in mot1:
+          i -= 1
+          if char == mot2[i]:
+            pass
+          else:
+            return False
+
     return True
 
+
 def contains_doubles(items: list) -> bool:
-    if len(items) == len(set(items)):
-        return False
-    else:
+    for elem in items:
+      if items.count(elem) > 1:
         return True
+    
+    return False
+
 
 def best_grades(student_grades: dict) -> dict:
+    # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
+    best_grade = {}
     highest_average = 0
     for key in student_grades:
-        average = sum(student_grades[key]) / len(student_grades[key])
-        if average > highest_average:
-            highest_average = average
-            meilleur = {}
-            meilleur[key] = average
-    return meilleur
+      average = sum(student_grades[key]) / len(student_grades[key])
+      if average > highest_average:
+        highest_average = average
+        best_grade = {}
+        best_grade[key] = average
+    return best_grade
+
 
 def frequence(sentence: str) -> dict:
+    # TODO: Afficher les lettres les plus fréquentes
+    #       Retourner le tableau de lettres
     wordcount = {}
     wordlist = list(sentence)
     for elem in wordlist:
-        wordcount[elem] = wordlist.count(elem)
+      wordcount[elem] = wordlist.count(elem)
+    
     return sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
 
-
 def get_recipes():
+    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
     recipe = {}
     nom = str(input("Entrez le nom de la recette: "))
-    ingredients = str(input("Entrez les ingrédients séparés par ', ': ")).split(", ")
+    ingredients = str(input("Entrez les ingredients separes par ', ': ")).split(", ")
     recipe[nom] = ingredients
+    
     return recipe
+
+
 def print_recipe(ingredients) -> None:
+    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
+    
     try:
       print(ingredients[str(input("Entrez le nom de la recette: "))])
     except:
       return False
+
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
